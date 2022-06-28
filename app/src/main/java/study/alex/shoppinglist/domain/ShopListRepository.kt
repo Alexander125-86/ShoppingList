e52268e4302e989aaa73cc179c61dfdbf3326ada
Alexander125-86 <94919919+Alexander125-86@users.noreply.github.com>
2022-06-28 14:52:00 +0700

    added MainViewModel

--- app/src/main/java/study/alex/shoppinglist/domain/ShopListRepository.kt
+++ app/src/main/java/study/alex/shoppinglist/domain/ShopListRepository.kt
@@ -1,5 +1,7 @@
 package study.alex.shoppinglist.domain
 
+import androidx.lifecycle.LiveData
+
 interface ShopListRepository {
 
     fun addShopItem(shopItem: ShopItem)
@@ -10,6 +12,6 @@ interface ShopListRepository {
 
     fun getShopItem(shopItem: Int): ShopItem
 
-    fun getShopList(): List<ShopItem>
+    fun getShopList(): LiveData<List<ShopItem>>
 
 }
--- app/src/main/java/study/alex/shoppinglist/presentation/MainViewModel.kt
+++ app/src/main/java/study/alex/shoppinglist/presentation/MainViewModel.kt
@@ -1,18 +1,41 @@
 package study.alex.shoppinglist.presentation
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import study.alex.shoppinglist.data.ShopListRepositoryImpl
 import study.alex.shoppinglist.domain.DeleteShopItemUseCase
 import study.alex.shoppinglist.domain.EditShopItemUseCase
 import study.alex.shoppinglist.domain.GetShopItemUseCase
+import study.alex.shoppinglist.domain.GetShopListUseCase
+import study.alex.shoppinglist.domain.ShopItem
 
 class MainViewModel: ViewModel() {
 
     private  val repository = ShopListRepositoryImpl
 
-    private val getShopItemUseCase = GetShopItemUseCase(repository)
+    private val getShopListUseCase = GetShopListUseCase(repository)
     private  val deleteShopItemUseCase = DeleteShopItemUseCase(repository)
     private val editShopItemUseCase = EditShopItemUseCase(repository)
 
 
+    val shopList = getShopListUseCase.getShopList()
+
+
+
+    fun deleteShopItem(shopItem:ShopItem){
+        deleteShopItemUseCase.deleteShopItem(shopItem)
+
+    }
+
+    fun changeEnableState(shopItem: ShopItem){
+        val newItem = shopItem.copy(enabled = !shopItem.enabled)
+        editShopItemUseCase.editShopItem(newItem)
+
+    }
+
 }
+
+
+
+
